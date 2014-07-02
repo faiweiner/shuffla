758345d1687b06dd628ae6e640bbaaab59c6b9b9
fai.weiner <fai.weiner@gmail.com>
2014-07-02 20:30:22 +1000

    add facebook key and password

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, '665999610153865', 'f05fdab9e6b85d4498b551e63525f23c'
 end
